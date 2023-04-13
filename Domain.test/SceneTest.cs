8bb7371254aa0118d3784090bac9315d4607b9df
Ignacio Ramirez <ignacioramirez452@gmail.com>
2023-04-13 10:28:00 -0300

    Feature: Nombre unico. Clase Scene-Fase: Red

---
+++ Domain.test/SceneTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.test
+{
+    [TestClass]
+    public class SceneTest
+    {
+
+
+        [TestMethod]
+        public void ReturnsFalseIfNameisVoid()
+        {
+            string name = "";
+            Scene scene = new Scene();
+            bool result = scene.VerifyName(name);
+            Assert.IsFalse(result);
+        }
+
+
+
+
+    }
+}
