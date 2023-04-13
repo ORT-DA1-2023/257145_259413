192e4c37a68d23d05ec067154a86e7d2f22947f2
Ignacio Ramirez <ignacioramirez452@gmail.com>
2023-04-13 09:50:27 -0300

    Feature: Nombre no vacio. Clase Model-Fase: Red

---
+++ Domain.test/ModelTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.test
+{
+    [TestClass]
+    public class ModelTest
+    {
+
+        [TestMethod]
+        public void ReturnsFalseIfNameisVoid()
+        {
+            string name = "";
+            Model model = new Model();
+            bool result = model.VerifyName(name);
+            Assert.IsFalse(result);
+        }
+
+
+
+
+
+    }
+}
