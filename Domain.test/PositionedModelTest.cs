6625c986b1f43e57fa0d056c9e8d5250b50e1ee4
Ignacio Ramirez <ignacioramirez452@gmail.com>
2023-04-14 14:37:34 -0300

    Feature: Verrifica si es decimal. Clase Coordinate/ PositionedModel-Fase: Red

--- Domain.test/PositionedModelTest.cs
+++ Domain.test/PositionedModelTest.cs
@@ -9,5 +9,26 @@ namespace Domain.test
     [TestClass]
     public class PositionedModelTest
     {
+
+
+        [TestMethod]
+        public void ReturnsFalseIfNotDecimalNUmber()
+        {
+            float x = 1;
+            float y = 1;
+            float z = 1;
+            Coordinate coordinate = new Coordinate();
+            bool result = coordinate.VerifyCoordinate(x,y,z);
+            Assert.IsFalse(result);
+
+        }
+
+
+
+
+
+
+
+
     }
 }
---
+++ Domain/Coordinate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Coordinate
+    {
+
+        public float x;
+        public float y;
+        public float z;
+
+        public Coordinate()
+        {
+          
+        }
+
+
+        public Coordinate(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+
+        }
+
+        public bool VerifyCoordinate(float x, float y, float z)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
