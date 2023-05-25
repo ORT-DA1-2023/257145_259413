dd6423c9f06d304fd20761b20d7ea50f4b6790fd
Ignacio Ramirez <ignacioramirez452@gmail.com>
2023-05-25 15:00:11 -0300

    feature: Terminado Requirimiento RF21

--- Domain/Material.cs
+++ Domain/Material.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -12,33 +13,50 @@ namespace Domain
     {
         public string name;
         public Color color;
-        public string type;
         public decimal blurred;
+        public string type;
+
+        
+
+        public static Material createMaterial( string selectedMaterial, string name, Color color, decimal valueMetalic)
+        {
+            switch (selectedMaterial)
+            {
+                case "Metálico":
+                    return new MetalicMaterial(name, color, valueMetalic);
+                  
+                    break;
+                default:
+                    return new LambertianoMaterial(name, color);
+                    break;
+            }
+
+
+        }
+
+
+
+        //Material mater=Material.create(lambertiano)
+
         public Material() 
         {
         
         }
-
+   
         public Material(string name, Color color)
         {
             this.name = name;
             this.color = color;
         }
-
-        public Material(string name, Color color, string type)
+    
+     
+        public Material(string name, Color color, decimal blurred)
         {
             this.name = name;
             this.color = color;
-            this.type = type;
-        }
-        //AGREGUE ESTO
-        public Material(string name, Color color, string type, decimal blurred)
-        {
-            this.name = name;
-            this.color = color;
-            this.type = type;
             this.blurred = blurred;
         }
+   
         public bool VerifyColor(int red, int green, int blue)
         {
             if(red < 0 || green < 0 || blue < 0 || red >255 || green > 255 || blue > 255)
@@ -47,7 +65,7 @@ namespace Domain
             }
             return true;
         }
-
+       
         public bool VerifyName(string name)
         {
             if(name.Length == 0 || !Regex.IsMatch(name, @"^[^\s].*[^\s]$"))
@@ -56,17 +74,23 @@ namespace Domain
             }
             return true;
         }
-        //VALIDAR QUE SEA MAYOR A 0 0 
-
-
-        //  public bool VeryfyBlurred(int value)
-        //{
-        //    if (value>=)
-        // }
+         
+        public bool VeryfyBlurred(decimal value)
+        {
+            if (value >= 0.0m)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
         public override string ToString()
         {
             return "R: " + color.R + " G: " + color.G + " B: " + color.B;
         }
+
     }
 }
---
+++ Domain/MetalicMaterial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class MetalicMaterial:Material
+    {
+
+        public MetalicMaterial()
+        {
+        }
+
+       // public MetalicMaterial(string name, Color color, decimal blurred)
+        //{
+         //   this.name = name;
+           // this.color = color;
+            //this.blurred = blurred;
+       // }
+
+
+       public MetalicMaterial(string name,Color color,decimal blurred) : base(name, color, blurred)
+        {
+            
+        }
+
+
+
+        public override string ToString()
+        {
+            return "R: " + color.R + " G: " + color.G + " B: " + color.B;
+        }
+
+
+
+
+    }
+}
